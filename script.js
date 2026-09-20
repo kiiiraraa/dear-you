f068aa691f38ab4a0ff4840884c1efca01c9a2ea
kiiiraraa <hnk.prv.1204@gmail.com>
2026-09-20 10:54:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-window.addEventListener("scroll", () => {
-    const scroll = window.scrollY;
-    const maxScroll = document.body.scrollHeight - window.innerHeight;
-    const progress = scroll / maxScroll;
 
-    if (progress < 0.33) {
-        document.body.style.backgroundColor = "#f8f5f2";
-    } else if (progress < 0.66) {
-        document.body.style.backgroundColor = "#e8e8f2";
-    } else {
-        document.body.style.backgroundColor = "#ddd9e8";
-    }
-});
