3740f8544fd2ae42be06c0b44c31ab3c359fb166
kiiiraraa <hnk.prv.1204@gmail.com>
2026-09-20 11:06:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.addEventListener("scroll", () => {
-    document.body.style.backgroundColor = "#e8e8f2";
+    const progress =
+        window.scrollY /
+        (document.body.scrollHeight - window.innerHeight);
+
+    const start = [238, 232, 226];
+    const end = [190, 188, 220];
+
+    const r = Math.round(start[0] + (end[0] - start[0]) * progress);
+    const g = Math.round(start[1] + (end[1] - start[1]) * progress);
+    const b = Math.round(start[2] + (end[2] - start[2]) * progress);
+
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 });
